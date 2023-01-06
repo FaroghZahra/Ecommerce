ebcb240536516cadfbb3d43800c293825695493e
FaroghZahra <syeda.farogh.zahra@gmail.com>
2023-01-06 19:53:19 +0500

    my commit

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('category/<slug:category_slug>', views.home, name = 'products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('cart', views.cart_detail, name= 'cart_detail'),
+    path('cart/add/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('cart/delete/<int:product_id>/', views.cart_delete, name='cart_delete'),
+    path('thankyou/<int:order_id>', views.thanks_page, name='thanks_page'),
+    path('search/', views.search, name='search'),
+]
